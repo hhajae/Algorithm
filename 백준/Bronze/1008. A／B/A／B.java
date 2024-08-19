04c6bb874de1325206ff811997280822905084b7
hhajae <84849611+hhajae@users.noreply.github.com>
2024-08-19 17:58:21 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		String s = br.readLine();
+		st = new StringTokenizer(s, " ");
+		
+		int num1 = Integer.parseInt(st.nextToken());
+		int num2 = Integer.parseInt(st.nextToken());
+		
+		System.out.println((num1*1.0)/num2);
+	    }
+}
