bb6701547d29c1fb5b47c09b63dcbafe9b63c267
hhajae <84849611+hhajae@users.noreply.github.com>
2024-08-20 15:52:25 +0900

    [Silver III] Title: 돌 게임 3, Time: 68 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        if(N == 1 || (3 <= N && N <= 6)) {
+        	bw.write("SK"); bw.flush(); bw.close(); br.close(); return;
+        }
+        if(N == 2 || N == 7) {
+        	bw.write("CY"); bw.flush(); bw.close(); br.close(); return;
+        }
+        if(N % 7 == 1 || N % 7 == 3 || N % 7 == 4 || N % 7 == 5 || N % 7 == 6) {
+        	sb.append("SK");
+        }
+        else {
+        	sb.append("CY");
+        }
+        
+        bw.write(sb.toString());
+		bw.flush();
+	    bw.close();
+	    br.close();
+	}
+
+}
