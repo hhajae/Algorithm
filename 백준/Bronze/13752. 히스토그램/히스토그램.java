69cb5ea37da7d66c3a49126b0d3838f386248cd2
hhajae <84849611+hhajae@users.noreply.github.com>
2024-08-20 15:22:22 +0900

    [Bronze IV] Title: 히스토그램, Time: 68 ms, Memory: 11684 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    StringTokenizer st = null;
+	    StringBuilder sb = new StringBuilder();
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    for (int i = 0; i < N; i++) {
+	    	int n = Integer.parseInt(br.readLine());
+			for (int j = 0; j < n; j++) {
+				sb.append("=");
+			}
+			sb.append("\n");
+		}
+	    
+	    bw.write(sb.toString());
+		bw.flush();
+	    bw.close();
+	    br.close();
+	}
+}
