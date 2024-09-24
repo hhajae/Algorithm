db7816c4f8a568d8ed6d189e8afaee2588b07a4c
hhajae <84849611+hhajae@users.noreply.github.com>
2024-09-25 08:53:16 +0900

    [Bronze V] Title: HI-ARC=?, Time: 72 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+        st = new StringTokenizer(s, " ");
+        int H = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        sb.append((H*I) - (A*R*C));
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
